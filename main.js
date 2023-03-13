f7f71dc9e0321aea3eb9fc7d8d4a57b6a336a991
Renan Souza <renansouza0122@gmail.com>
2023-03-13 19:09:40 -0300

    display weekday date and hour on the html and added  function to format hours

--- main.js
+++ main.js
@@ -11,6 +11,22 @@ const week_days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","
 const weekDay = week_days[now.getDay()];
 
 const month_name = months[now.getMonth()];
+const month_number = now.getMonth();
 const day = now.getDate();
 const year = now.getFullYear()
 
+
+displayWeekDay.innerHTML = weekDay;
+displayDate.innerHTML = `${month_number + 1}/${day}/${year}`;
+displayHour.innerHTML = formatAMPM();
+
+function formatAMPM(){
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    const ampm = hours >= 12 ? "PM":"AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    minutes = minutes < 10 ? "0" + minutes: minutes;
+    let strTime = `${hours}:${minutes}:${ampm}`;
+    return strTime
+}
