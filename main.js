4413fe47d7de4efb170bca0f7f402ecd247809ad
Renan Souza <renansouza0122@gmail.com>
2023-03-13 17:43:24 -0300

    variables to  month , day and year added

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ const displayWeekDay = document.querySelector(".weekday");
 const displayDate = document.querySelector(".date");
 const displayHour = document.querySelector(".hour");
 
-let d = new Date();
-let day = d.getDate();
+const now = new Date();
 
+const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
 const week_days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
-const weekDay = week_days[d.getDay()];
+
+const weekDay = week_days[now.getDay()];
+
+const month_name = months[now.getMonth()];
+const day = now.getDate();
+const year = now.getFullYear()
+
