df5c4f8573205c81e629639446f1a04bfd7fa14b
Renan Souza <renansouza0122@gmail.com>
2023-03-13 17:25:51 -0300

    weekday added

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const displayWeekDay = document.querySelector(".weekday");
 const displayDate = document.querySelector(".date");
 const displayHour = document.querySelector(".hour");
+
+let d = new Date();
+let day = d.getDate();
+
+const week_days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+const weekDay = week_days[d.getDay()];
